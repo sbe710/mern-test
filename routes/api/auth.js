92fb9ac10bf7169a03fc1dad553d5ad268f908de
alep0518 <aleksandr.panasenko@netcracker.com>
2020-03-26 23:17:29 +0300

    Added jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// @route  GET api//auth
+// @route  GET api/auth
 // @desc   Test route
 // @access Public
 router.get("/", (req, res) => res.send("Auth route"));
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
+const config = require("config");
 
-// @route  POST api//users
+// @route  POST api/users
 // @desc   Register user
 // @access Public
 router.post("/", [
@@ -50,7 +51,21 @@ router.post("/", [
         await user.save();
 
         // Return jwt
-        res.send("User registered");
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            }
+        );
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server error")
