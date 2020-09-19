05c85429cef257ff20ef63435e715ec28663017a
alep0518 <aleksandr.panasenko@netcracker.com>
2020-09-19 13:38:40 +0300

    Refactor

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
 import PropTypes from 'prop-types';
