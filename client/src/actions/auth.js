2989f228728c45befd552c6a8d135569c63c3b04
alep0518 <aleksandr.panasenko@netcracker.com>
2020-09-21 18:08:02 +0300

    Logout [43]

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS,
+    LOGOUT
 } from './constants';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -92,4 +93,9 @@ export const login = ({ email, password }) => async dispatch => {
             type: LOGIN_FAIL
         })
     }
+}
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 }
