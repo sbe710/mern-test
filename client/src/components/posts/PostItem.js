b99ad46289c6f2d71f950572cf98bcd517c4d496
alep0518 <aleksandr.panasenko@netcracker.com>
2020-09-25 01:32:47 +0300

    Fix

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -25,7 +25,7 @@ const PostItem = ({
     <Fragment>
         <div className='post bg-white p-1 my-1'>
             <div>
-                <Link to={`/profile/${auth.user._id}`}>
+                <Link to={`/profile/${user}`}>
                     <img
                         className='round-img'
                         src={avatar}
