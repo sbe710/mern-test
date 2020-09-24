a5c3ac38c029289fd87182ab10f4f1614f8be0ca
alep0518 <aleksandr.panasenko@netcracker.com>
2020-09-24 18:23:40 +0300

    Post reducer [61]

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
@@ -46,6 +47,7 @@ const App = () => {
                             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
                             <PrivateRoute exact path='/add-experience' component={AddExperience} />
                             <PrivateRoute exact path='/add-education' component={AddEducation} />
+                            <PrivateRoute exact path='/posts' component={Posts} />
                         </Switch>
                     </section>
                     <Route />
